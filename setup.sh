b2232034b0d06739a23e8aa963636061cbc4927e
cartermak <cartermak3@gmail.com>
2020-01-14 00:20:09 -0700

    remove pypdf2 from setup

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ PY_INSTALLED=$(python3 --version|grep "Python 3.*")
 
 # If not installed, install latest version
 if [ "" == "$PY_INSTALLED" ]; then
-    echo "Python 3 not found; installing Python 3.6..."
-    sudo apt-get --force-yes --yes install python3.6
+    echo "Python 3 not found; installing Python 3.7..."
+    sudo apt-get --force-yes --yes install python3.7
 else
     echo "Python found."
 fi
@@ -20,18 +20,6 @@ if [ "" == "$PIP_INSTALLED" ]; then
     sudo apt-get --force-yes --yes install python-pip
 else
     echo "Pip found."
-fi
-
-
-# Check if PyPDF2 is installed
-PDF_INSTALLED=$(pip list --format columns|grep "PyPDF2")
-
-# If not installed, install latest version
-if [ "" == "$PDF_INSTALLED" ]; then
-    echo "PyPDF2 not found; installing PyPDF2..."
-    pip install pypdf2
-else
-    echo "PyPDF2 found."
 fi
 
 echo "Setup complete."
